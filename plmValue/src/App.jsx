4c645e2516d3599ce1dfb59432f6363b2c1eb3a9
Jason Lin <linjay@umich.edu>
2024-07-25 11:22:39 -0400

    Created starter page for plm_value

---
+++ plmValue/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="chat">
+        <div className='pageHeader'></div>
+        <div className='chatScreen'></div>
+        <div className='textInput'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
